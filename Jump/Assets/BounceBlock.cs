dc2089fc78e9f54edf184239886cde21ee7da952
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2023-08-15 14:21:28 +0900

    ChangeBounceBlock

--- Jump/Assets/BounceBlock.cs
+++ Jump/Assets/BounceBlock.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public class BounceBlock : MonoBehaviour
 {
-    [SerializeField] public float force;
+    [SerializeField] public bool toTarget;
+    [SerializeField] public Transform target;
     [SerializeField] [Range(0f, 360f)] public float deg;
+    [SerializeField] public float force;
     private float directionX;
     private float directionY;
+    private Vector2 direction;
+    private Vector2 bounceVector;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            directionX = Mathf.Sin(ConvertDegToRad(deg));
-            directionY = Mathf.Cos(ConvertDegToRad(deg));
-            Vector2 direction = new Vector2(directionX, directionY);
-            Vector2 bounceVector = direction.normalized * force;
-            collision.rigidbody.AddForce(bounceVector, ForceMode2D.Impulse);
+            BouncingPlayer(collision.rigidbody);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            BouncingPlayer(collision.gameObject.GetComponent<Rigidbody2D>());
         }
     }
 
     float ConvertDegToRad(float deg)
     {
         return (deg * (Mathf.PI / 180));
+    }
+
+    void BouncingPlayer(Rigidbody2D rg)
+    {
+        rg.velocity = Vector2.zero;
+        if (toTarget)
+        {
+            directionX = target.position.x - transform.position.x;
+            directionY = target.position.y - transform.position.y;
+        }
+        else
+        {
+            directionX = Mathf.Cos(ConvertDegToRad(deg));
+            directionY = Mathf.Sin(ConvertDegToRad(deg));
+        }
+        direction = new Vector2(directionX, directionY);
+        bounceVector = direction.normalized * force;
+        rg.AddForce(bounceVector, ForceMode2D.Impulse);
     }
 
 }
