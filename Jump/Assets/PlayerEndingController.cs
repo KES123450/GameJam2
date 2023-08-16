fb2018d675b12458c65bfe2f3fb3f031a5b8a263
김은서 <a55208806@gmail.com>
2023-08-17 01:00:53 +0900

    GOLD

--- Jump/Assets/PlayerEndingController.cs
+++ Jump/Assets/PlayerEndingController.cs
@@ -12,6 +12,11 @@ public class PlayerEndingController : MonoBehaviour
     public Vector3 endingPos;
     public GameManager GameManager;
 
+    public Transform leg1;
+    public Transform leg2;
+
+    private bool isWalking = false;
+    private bool isEnd=false;
     void Start()
     {
         endSequence = DOTween.Sequence().Pause()
@@ -23,10 +28,34 @@ public class PlayerEndingController : MonoBehaviour
             .Append(creditText.DOFade(0f, 3f))
             .OnComplete(() => GameManager.ActiveEndUI());
 
+
+    }
+
+    private void MoveLeg(Transform leg1, Transform leg2, float MoveAngleAbs)
+    {
+        isWalking = true;
+        leg1.DOLocalRotate(new Vector3(0f, 0f, MoveAngleAbs), 0.1f).OnComplete(() => leg1.DOLocalRotate(new Vector3(0f, 0f, -MoveAngleAbs),0.1f).OnComplete(() => isWalking = false));
+        leg2.DOLocalRotate(new Vector3(0f, 0f, -MoveAngleAbs), 0.1f).OnComplete(() => leg2.DOLocalRotate(new Vector3(0f, 0f, MoveAngleAbs), 0.1f).OnComplete(() => isWalking = false));
     }
 
     public void EndingPlay()
     {
+        isEnd = true;
+        
         endSequence.Play();
+        
     }
+
+    private void Update()
+    {
+        if (isEnd)
+        {
+            if (!isWalking)
+            {
+                MoveLeg(leg1, leg2, 10f);
+            }
+        }
+        
+    }
+
 }
